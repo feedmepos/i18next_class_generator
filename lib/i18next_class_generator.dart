dbaf31192e2259593c34f7e4b0a256f0e424f7ca
mingern789 <krrmol99@hotmail.com>
2022-04-08 19:54:41 +0800

    WIP

--- lib/i18next_class_generator.dart
+++ lib/i18next_class_generator.dart
@@ -3,8 +3,11 @@
 // BSD-style license that can be found in the LICENSE file.
 
 // ignore: import_of_legacy_library_into_null_safe
+import 'dart:io';
+
 import 'package:build/build.dart';
-import 'package:path/path.dart' show join, basenameWithoutExtension;
+import 'package:path/path.dart' show basenameWithoutExtension;
+import 'package:glob/glob.dart';
 
 Builder i18NextClassGenerator(BuilderOptions options) =>
     I18NextClassGenerator();
@@ -12,17 +15,29 @@ Builder i18NextClassGenerator(BuilderOptions options) =>
 class I18NextClassGenerator implements Builder {
   @override
   Future build(BuildStep buildStep) async {
-    // Each [buildStep] has a single input.
-    AssetId currentAsset = buildStep.inputId;
-    print('discover ${buildStep.inputId}');
-    var filename = basenameWithoutExtension(buildStep.inputId.toString());
-    await buildStep.writeAsString(
-        new AssetId(currentAsset.package, 'lib/i18next/$filename.i18next.dart'),
-        '');
+    print('building');
+    final exports = buildStep.findAssets(Glob('**/*.json'));
+    print(exports);
+
+    await for (var exportLibrary in exports) {
+      print(exportLibrary.uri);
+    }
+
+    // AssetId currentAsset = buildStep.inputId;
+    // var copy = currentAsset.changeExtension('.dart');
+    // print('discover ${buildStep.inputId}');
+    // var filename = basenameWithoutExtension(buildStep.inputId.toString());
+    // // await buildStep.writeAsString(copy, 'lib/i18next/$filename.i18next.dart');
+    // File file = await File('lib/i18next/$filename.i18next.dart');
+    // await file.writeAsString('abc');
+
+    // await buildStep.writeAsString(
+    //     new AssetId(currentAsset.package, 'lib/i18next/$filename.i18next.dart'),
+    //     '');
   }
 
   @override
   final buildExtensions = const {
-    '.json': ['.i18next.dart']
+    r'$i18next$': ['.i18next.dart']
   };
 }
